8f319c16cefba0ef567e975dbb0a19751bb30701
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-17 08:49:56 -0300

    Item 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,14 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemons } from '../pages';
+
+describe('Testa o componente <FavoritePokemons.js />', () => {
+  it(`Testa se é exibida na tela a mensagem
+    'No favorite pokemon found', caso a pessoa
+      não tenha pokémons favoritos`, () => {
+    renderWithRouter(<FavoritePokemons />);
+    const notFoundPokemonText = screen.getByText(/no favorite pokemon found/i);
+    expect(notFoundPokemonText).toBeInTheDocument();
+  });
+});
